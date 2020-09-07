bd1977b312eac36d299080f206c21736e9f47c2e
Simon Woodworth <s.woodworth@ucc.ie>
2020-09-07 13:21:14 +0100

    Add project files.

---
+++ MvcXmlDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcXmlDemo.Models;
+
+namespace MvcXmlDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Home/Index
+        [HttpPost]
+        public IActionResult Index(Category cat)
+        {
+            return View("Save", cat);
+        }
+    }
+}
